9ebedd1b25fbbbf7ca0d6641083f97f0f0be2239
Amos Neculau <contact@amosneculau.ro>
2019-03-23 10:45:50 +0100

    Initial Commit

---
+++ src/main/java/com/advanced/software/engineering/aseproject/AseprojectApplication.java
@@ -0,0 +1,13 @@
+package com.advanced.software.engineering.aseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AseprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AseprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/advanced/software/engineering/aseproject/AseprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.advanced.software.engineering.aseproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AseprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
