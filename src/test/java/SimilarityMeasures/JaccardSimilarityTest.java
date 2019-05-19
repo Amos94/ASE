d4ca894d26b47f6b180f8a4e7616bfeb087467b9
leonardas103 <wingdragon_9@hotmail.com>
2019-05-19 18:27:42 +0200

    added Jaccard Simalarity test

--- src/test/java/SimilarityMeasures/JaccardSimilarityTest.java
+++ src/test/java/SimilarityMeasures/JaccardSimilarityTest.java
@@ -2,11 +2,34 @@ package SimilarityMeasures;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 public class JaccardSimilarityTest {
 
     @Test
     public void computeSimilarity() {
+        int[] a = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+        int[] b = new int[]{ 1,2,3,4,5,6 };
+
+        Set<Integer> indexSet = new HashSet<>();
+        for (int value : a) {
+            indexSet.add(value);
+        }
+        Set<Integer> querySet = new HashSet<>();
+        for (int value : b) {
+            querySet.add(value);
+        }
+
+        Set<Integer> intersection = new HashSet<>(indexSet);
+        intersection.retainAll(querySet);
+
+        Set<Integer> union = new HashSet<>(indexSet);
+        union.addAll(querySet);
+
+        assertEquals((double) b.length/a.length,(double) intersection.size() / (double) union.size(), 0);
+
     }
 }
--- src/test/java/com/advanced/software/engineering/aseproject/RecommenderInitializationTest.java
+++ src/test/java/com/advanced/software/engineering/aseproject/RecommenderInitializationTest.java
@@ -1,14 +1,27 @@
 package com.advanced.software.engineering.aseproject;
 
+import Context.ContextHelper;
 import Context.IoHelper;
+import Index.IInvertedIndex;
+import Index.InvertedIndex;
 import Utils.Configuration;
 import org.junit.Test;
 
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotEquals;
 
 public class RecommenderInitializationTest {
+
+    @Test
+    public void RecommenderInitialization(){
+        assertEquals("com.advanced.software.engineering.aseproject.RecommenderInitialization", RecommenderInitialization.class.getName());
+        Logger logger  = Logger.getLogger(RecommenderInitialization.class.getName());
+        assertNotEquals(null,logger);
+    }
 
     @Test
     public void createIndex() {
@@ -16,6 +29,18 @@ public class RecommenderInitializationTest {
         Set<String> zips = IoHelper.findAllZips(Configuration.CONTEXTS_DIR);
         int numberOfZips = zips.size();
         assertNotEquals(0,numberOfZips);
+
+        ContextHelper ctxHelper = new ContextHelper(Configuration.CONTEXTS_DIR);
+        assertNotEquals(null, ctxHelper);
+
+        long startTime = System.currentTimeMillis();
+        assertNotEquals(null, startTime);
+
+        IInvertedIndex invertedIndex = new InvertedIndex(Configuration.INDEX_STORAGE);
+        assertNotEquals(null, invertedIndex);
+
+        Model model = new Model(invertedIndex);
+        assertNotEquals(null, model);
     }
 
     @Test
